397f214815217f124e244c5d67c02e288689623e
Daniel-Cross <crossy1686@gmail.com>
2018-08-16 16:50:44 +0100

    added form

--- src/components/add-property.jsx
+++ src/components/add-property.jsx
@@ -1,40 +1,83 @@
 import React from 'react';
+import '../styles/add-property.css';
 
 class AddProperty extends React.Component {
   constructor() {
     super();
     this.state = {
-      title: '',
-      type: '',
+      fields: {
+        title: '',
+        price: '',
+        location: '',
+        email: '',
+      },
     };
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange(event) {
-    this.setState({ value: event.target.value });
-  }
-
-  handleSubmit(event) {
-    alert('Thanks for the submission!: ' + this.state.value);
+  handleAddProperty = event => {
     event.preventDefault();
-  }
+
+    console.log(this.state.fields);
+  };
+
+  handleFieldChange = event => {
+    this.setState({
+      fields: {
+        [event.target.name]: event.target.value,
+      },
+    });
+  };
 
   render() {
     return (
       <div className="AddProperty">
-        <form onSubmit={this.handleSubmit}>
+        <form onSubmit={this.handleAddProperty}>
           <label className="title">
             Title:
-            <input type="text" value={this.state.title} onChange={this.handleChange} />
+            <input type="text" value={this.state.fields.title} onChange={this.handleFieldChange} />
           </label>
           <label className="type">
             Type of Property:
-            <input type="text" value={this.state.type} onChange={this.handleChange} />
+            <select type="home-list" form="house-form">
+              <option value="house">House</option>
+              <option value="apartment">Apartment</option>
+              <option value="bungalow">Bungalow</option>
+              <option value="other">Other</option>
+            </select>
           </label>
-
-          <input type="submit" value="Submit" />
+          <label className="bedrooms">
+            Number of Bedrooms:
+            <select type="bedrooms" form="bedroom-form">
+              <option value="studio">Studio</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="+4">>4</option>
+            </select>
+          </label>
+          <label className="bathrooms">
+            Number of Bathrooms:
+            <select type="bathrooms" form="bathroom-form">
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="3">>3</option>
+            </select>
+          </label>
+          <label className="price">
+            Value of Property:
+            <input type="text" value={this.state.price} onChange={this.handleChange} />
+          </label>
+          <label className="location">
+            Location:
+            <input type="text" value={this.state.location} onChange={this.handleChange} />
+          </label>
+          <label className="email">
+            Email:
+            <input type="text" value={this.state.email} onChange={this.handleChange} />
+          </label>
+          <button type="submit" value="Add">Add</button>
         </form>
       </div>
     );
