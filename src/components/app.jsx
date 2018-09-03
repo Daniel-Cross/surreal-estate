c97a25d84c02a82bb4f5f0a1fb43729139363b9a
Daniel-Cross <crossy1686@gmail.com>
2018-09-03 23:31:03 +0100

    added facebook component

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,21 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../styles/app.css';
 import Navbar from './navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Properties from './properties';
 import AddProperty from './add-property';
 
-const App = ({}) => (
-  <Router>
-    <div className="App">
-      <Navbar />
-      <Switch>
-        <Route exact path="/" component={Properties} />
-        <Route exact path="/add-property" component={AddProperty} />
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      userID: null,
+      name: '',
+      email: '',
+      picture: ''
+    };
+  }
 
-      </Switch>
-    </div>
-  </Router>
-);
+  handleLogin = response => {
+    console.log(response);
+  };
+
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Navbar
+            onLogin={this.handleLogin()}
+            userID={this.state.userID}
+            name={this.state.name}
+            email={this.state.email}
+            picture={this.state.picture}
+          />
+          <Switch>
+            <Route exact path="/" component={Properties} />
+            <Route exact path="/add-property" component={AddProperty} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
 
 export default App;
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,20 +1,42 @@
 import React from 'react';
 import '../styles/navbar.css';
 import { Link } from 'react-router-dom';
+import FacebookLogin from 'react-facebook-login';
 
-const Navbar = ({}) => (
-  <div className="Navbar">
-    <div className="logo">
-      <i className="fas fa-globe-americas" />
-      <span className="header">Surreal Estate</span>
+const Navbar = props => {
+  let showButton;
+
+  if (!props.userID) {
+    showButton = (
+      <FacebookLogin
+        appId="2245734235706702"
+        fields="name,email,picture"
+        callback={props.onLogin}
+      />
+    );
+  } else {
+    showButton = <button className="signOut">Sign Out</button>;
+  }
+
+  return (
+    <div className="Navbar">
+      <div className="logo">
+        <i className="fas fa-globe-americas" />
+        <span className="header">Surreal Estate</span>
+      </div>
+      <div className="nav">
+        <ul>
+          <Link className="item" to="/">
+            <i className="fas fa-home" /> View Properties
+          </Link>
+          <Link className="item" to="/add-property">
+            <i className="fab fa-fort-awesome" /> Add New Property
+          </Link>
+          <span>{showButton}</span>
+        </ul>
+      </div>
     </div>
-    <div className="nav">
-      <ul>
-        <Link className="item" to="/"><i className="fas fa-home" /> View Properties</Link>
-        <Link className="item" to="/add-property"><i className="fab fa-fort-awesome" /> Add New Property</Link>
-      </ul>
-    </div>
-  </div>
-);
+  );
+};
 
 export default Navbar;
