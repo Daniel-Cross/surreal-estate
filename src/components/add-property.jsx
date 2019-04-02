a51d421d8a6dd1768d3edebb0b94635927d14360
Daniel Cross <crossy1686@gmail.com>
2019-04-02 07:51:10 +0200

    updated post endpoint

--- src/components/add-property.jsx
+++ src/components/add-property.jsx
@@ -4,159 +4,155 @@ import axios from 'axios';
 import Alert from './alert';
 
 class AddProperty extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      fields: {
-        title: '',
-        type: 'Flat',
-        city: 'Manchester',
-        bedrooms: '0',
-        bathrooms: '0',
-        price: '0',
-        email: '',
-        alertMessage: '',
-        isSuccess: false,
-        isError: false,
-      },
-    };
-  }
+	constructor() {
+		super();
+		this.state = {
+			fields: {
+				title: '',
+				type: 'Flat',
+				city: 'Manchester',
+				bedrooms: '0',
+				bathrooms: '0',
+				price: '0',
+				email: '',
+				alertMessage: '',
+				isSuccess: false,
+				isError: false,
+			},
+		};
+	}
 
-  handleAddProperty = event => {
-    event.preventDefault();
-    this.setState({
-      alertMessage: '',
-      isSuccess: false,
-      isError: false,
-    });
-    console.log(this.state.fields);
+	handleAddProperty = (event) => {
+		event.preventDefault();
+		this.setState({
+			alertMessage: '',
+			isSuccess: false,
+			isError: false,
+		});
+		console.log(this.state.fields);
 
-    axios
-      .post(
-        'http://localhost:3000/api/v1/PropertyListing',
-        /* title: this.state.title,
+		axios
+			.post(
+				'https://surreal-estate-db.herokuapp.com/api/v1/PropertyListing',
+				/* title: this.state.title,
       type: this.state.type,
       city: this.state.city,
       bedrooms: this.state.bedrooms,
       bathrooms: this.state.bathrooms,
       price: this.state.price,
       email: this.state.email, */
-        this.state.fields
-      )
-      .then(() => this.setState({ isSuccess: true, alertMessage: 'Property added.' }))
-      .catch(() => {
-        this.setState({
-          alertMessage: 'Server error. Please try again later',
-          isError: true,
-        });
-      });
-  };
+				this.state.fields
+			)
+			.then(() => this.setState({ isSuccess: true, alertMessage: 'Property added.' }))
+			.catch(() => {
+				this.setState({
+					alertMessage: 'Server error. Please try again later',
+					isError: true,
+				});
+			});
+	};
 
-  handleFieldChange = event => {
-    this.setState({
-      fields: {
-        ...this.state.fields,
-        [event.target.name]: event.target.value,
-      },
-    });
-  };
+	handleFieldChange = (event) => {
+		this.setState({
+			fields: {
+				...this.state.fields,
+				[event.target.name]: event.target.value,
+			},
+		});
+	};
 
-  render() {
-    return (
-      <div className="AddProperty">
-        <form onSubmit={this.handleAddProperty}>
-          {this.state.isSuccess && (
-            <Alert message={this.state.alertMessage} success />
-          )}
-          {this.state.isError && <Alert message={this.state.alertMessage} />}
-          <label className="title">
-            Title:
-            <input
-              className="title-input"
-              type="text"
-              name="title"
-              value={this.state.fields.title}
-              onChange={this.handleFieldChange}
-            />
-          </label>
-          <label className="type">
-            Type of Property:
-            <select
-              className="type-select"
-              name="type"
-              value={this.state.fields.type}
-              onChange={this.handleFieldChange}
-            >
-              <option value="flat">Flat</option>
-              <option value="detached">Detached</option>
-              <option value="semi-detached">Semi-Detached</option>
-              <option value="terraced">Terraced</option>
-              <option value="end-of-terrace">End of Terrace</option>
-              <option value="cottage">Cottage</option>
-              <option value="bungalow">Bungalow</option>
-            </select>
-          </label>
-          <label className="city">
-            Location:
-            <select
-              className="city-select"
-              name="city"
-              value={this.state.fields.city}
-              onChange={this.handleFieldChange}
-            >
-              <option value="Manchester">Manchester</option>
-              <option value="Leeds">Leeds</option>
-              <option value="Sheffield">Sheffield</option>
-              <option value="Liverpool">Liverpool</option>
-            </select>
-          </label>
-          <label className="bedrooms">
-            Number of Bedrooms:
-            <input
-              className="bedroom-input"
-              type="number"
-              name="bedrooms"
-              value={this.state.fields.bedrooms}
-              onChange={this.handleFieldChange}
-            />
-          </label>
-          <label className="bathrooms">
-            Number of Bathrooms:
-            <input
-              className="bathroom-input"
-              type="number"
-              name="bathrooms"
-              value={this.state.fields.bathrooms}
-              onChange={this.handleFieldChange}
-            />
-          </label>
-          <label className="price">
-            Price (£):
-            <input
-              className="price-input"
-              type="number"
-              name="price"
-              value={this.state.fields.price}
-              onChange={this.handleFieldChange}
-            />
-          </label>
-          <label className="email">
-            Email Address:
-            <input
-              className="email-input"
-              type="email"
-              name="email"
-              value={this.state.fields.email}
-              onChange={this.handleFieldChange}
-            />
-          </label>
-          <button className="submit" type="submit" value="Add">
-            Add
-          </button>
-        </form>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="AddProperty">
+				<form onSubmit={this.handleAddProperty}>
+					{this.state.isSuccess && <Alert message={this.state.alertMessage} success />}
+					{this.state.isError && <Alert message={this.state.alertMessage} />}
+					<label className="title">
+						Title:
+						<input
+							className="title-input"
+							type="text"
+							name="title"
+							value={this.state.fields.title}
+							onChange={this.handleFieldChange}
+						/>
+					</label>
+					<label className="type">
+						Type of Property:
+						<select
+							className="type-select"
+							name="type"
+							value={this.state.fields.type}
+							onChange={this.handleFieldChange}>
+							<option value="flat">Flat</option>
+							<option value="detached">Detached</option>
+							<option value="semi-detached">Semi-Detached</option>
+							<option value="terraced">Terraced</option>
+							<option value="end-of-terrace">End of Terrace</option>
+							<option value="cottage">Cottage</option>
+							<option value="bungalow">Bungalow</option>
+						</select>
+					</label>
+					<label className="city">
+						Location:
+						<select
+							className="city-select"
+							name="city"
+							value={this.state.fields.city}
+							onChange={this.handleFieldChange}>
+							<option value="Manchester">Manchester</option>
+							<option value="Leeds">Leeds</option>
+							<option value="Sheffield">Sheffield</option>
+							<option value="Liverpool">Liverpool</option>
+						</select>
+					</label>
+					<label className="bedrooms">
+						Number of Bedrooms:
+						<input
+							className="bedroom-input"
+							type="number"
+							name="bedrooms"
+							value={this.state.fields.bedrooms}
+							onChange={this.handleFieldChange}
+						/>
+					</label>
+					<label className="bathrooms">
+						Number of Bathrooms:
+						<input
+							className="bathroom-input"
+							type="number"
+							name="bathrooms"
+							value={this.state.fields.bathrooms}
+							onChange={this.handleFieldChange}
+						/>
+					</label>
+					<label className="price">
+						Price (£):
+						<input
+							className="price-input"
+							type="number"
+							name="price"
+							value={this.state.fields.price}
+							onChange={this.handleFieldChange}
+						/>
+					</label>
+					<label className="email">
+						Email Address:
+						<input
+							className="email-input"
+							type="email"
+							name="email"
+							value={this.state.fields.email}
+							onChange={this.handleFieldChange}
+						/>
+					</label>
+					<button className="submit" type="submit" value="Add">
+						Add
+					</button>
+				</form>
+			</div>
+		);
+	}
 }
 
 export default AddProperty;
