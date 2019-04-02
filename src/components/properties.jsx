8fb45cfa935e1177b105cd8f3d7356972439a898
Daniel Cross <crossy1686@gmail.com>
2019-04-02 07:50:30 +0200

    updated api endpoint

--- src/components/properties.jsx
+++ src/components/properties.jsx
@@ -6,152 +6,114 @@ import { Link } from 'react-router-dom';
 import qs from 'qs';
 
 class Properties extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      properties: [],
-      isError: false,
-      alertMessage: '',
-      search: '',
-    };
-  }
+	constructor() {
+		super();
+		this.state = {
+			properties: [],
+			isError: false,
+			alertMessage: '',
+			search: '',
+		};
+	}
 
-  componentDidMount() {
-    axios
-      .get('http://localhost:3000/api/v1/PropertyListing')
-      .then(response => this.setState({ properties: response.data }))
-      .catch(() => {
-        this.setState({
-          isError: true,
-          alertMessage: 'Server Error, properties cannot be found',
-        });
-      });
-  }
+	componentDidMount() {
+		axios
+			.get('https://surreal-estate-db.herokuapp.com/api/v1/PropertyListing')
+			.then((response) => this.setState({ properties: response.data }))
+			.catch(() => {
+				this.setState({
+					isError: true,
+					alertMessage: 'Server Error, properties cannot be found',
+				});
+			});
+	}
 
-  componentDidUpdate(prevProps) {
-    const { search } = this.props.location;
+	componentDidUpdate(prevProps) {
+		const { search } = this.props.location;
 
-    if (prevProps.location.search !== search) {
-      axios
-        .get(`http://localhost:3000/api/v1/PropertyListing${search}`)
-        .then(({ data: properties }) => this.setState({ properties }))
-        .catch(() => {
-          this.setState({
-            isError: true,
-            alertMessage: 'Server Error, city not found',
-          });
-        });
-    }
-  }
+		if (prevProps.location.search !== search) {
+			axios
+				.get(`https://surreal-estate-db.herokuapp.com/api/v1/PropertyListing${search}`)
+				.then(({ data: properties }) => this.setState({ properties }))
+				.catch(() => {
+					this.setState({
+						isError: true,
+						alertMessage: 'Server Error, city not found',
+					});
+				});
+		}
+	}
 
-  buildQueryString = (operation, valueObj) => {
-    const { location: { search } } = this.props;
+	buildQueryString = (operation, valueObj) => {
+		const { location: { search } } = this.props;
 
-    const currentQueryParams = qs.parse(search, { ignoreQueryPrefix: true });
+		const currentQueryParams = qs.parse(search, { ignoreQueryPrefix: true });
 
-    const newQueryParams = {
-      ...currentQueryParams,
-      [operation]: JSON.stringify({
-        ...JSON.parse(currentQueryParams[operation] || '{}'),
-        ...valueObj,
-      }),
-    };
+		const newQueryParams = {
+			...currentQueryParams,
+			[operation]: JSON.stringify({
+				...JSON.parse(currentQueryParams[operation] || '{}'),
+				...valueObj,
+			}),
+		};
 
-    return qs.stringify(newQueryParams, { addQueryPrefix: true, encode: false });
-  };
+		return qs.stringify(newQueryParams, { addQueryPrefix: true, encode: false });
+	};
 
-  handleSearch = event => {
-    event.preventDefault();
+	handleSearch = (event) => {
+		event.preventDefault();
 
-    const { search } = this.state;
-    const newQueryString = this.buildQueryString('query', { title: { $regex: search } });
+		const { search } = this.state;
+		const newQueryString = this.buildQueryString('query', { title: { $regex: search } });
 
-    const { history } = this.props;
-    history.push(newQueryString);
-  };
+		const { history } = this.props;
+		history.push(newQueryString);
+	};
 
-  render() {
-    return (
-      <div className="sidebar">
-        <div className="sort-buttons">
-          <Link
-            className="manchester"
-            to={this.buildQueryString('query', { city: 'Manchester' })}
-          >
-            <button className="sorts">
-              Manchester
-            </button>
-          </Link>
-          <Link
-            className="leeds"
-            to={this.buildQueryString('query', { city: 'Leeds' })}
-          >
-            <button className="sorts">
-              Leeds
-            </button>
-          </Link>
-          <Link
-            className="sheffield"
-            to={this.buildQueryString('query', { city: 'Sheffield' })}
-          >
-            <button className="sorts">
-              Sheffield
-            </button>
+	render() {
+		return (
+			<div className="sidebar">
+				<div className="sort-buttons">
+					<Link className="manchester" to={this.buildQueryString('query', { city: 'Manchester' })}>
+						<button className="sorts">Manchester</button>
+					</Link>
+					<Link className="leeds" to={this.buildQueryString('query', { city: 'Leeds' })}>
+						<button className="sorts">Leeds</button>
+					</Link>
+					<Link className="sheffield" to={this.buildQueryString('query', { city: 'Sheffield' })}>
+						<button className="sorts">Sheffield</button>
+					</Link>
 
-          </Link>
+					<Link className="liverpool" to={this.buildQueryString('query', { city: 'Liverpool' })}>
+						<button className="sorts">Liverpool</button>
+					</Link>
 
-          <Link
-            className="liverpool"
-            to={this.buildQueryString('query', { city: 'Liverpool' })}
-          >
-            <button className="sorts">
-              Liverpool
-            </button>
+					<Link className="price-descending" to={this.buildQueryString('sort', { price: -1 })}>
+						<button className="sorts">Price Descending</button>
+					</Link>
 
-          </Link>
-
-          <Link
-            className="price-descending"
-            to={this.buildQueryString('sort', { price: -1 })}
-          >
-            <button className="sorts">
-              Price Descending
-            </button>
-          </Link>
-
-
-          <Link
-            className="price-ascending"
-            to={this.buildQueryString('sort', { price: +1 })}
-          >
-            <button className="sorts">
-              Price Ascending
-            </button>
-          </Link>
-          <form onSubmit={this.handleSearch}>
-            <input
-              className="title-search"
-              placeholder="Search Title..."
-              type="text"
-              value={this.state.search}
-              onChange={event => this.setState({ search: event.target.value })}
-            />
-            <button
-              className="search-button"
-              type="submit"
-            >
-              <i className="fas fa-search" />
-            </button>
-          </form>
-          <div className="properties">
-            {this.state.properties.map(property => (
-              <PropertyCard key={property._id} {...property} />
-            ))}
-          </div>
-        </div>
-      </div>
-    );
-  }
+					<Link className="price-ascending" to={this.buildQueryString('sort', { price: +1 })}>
+						<button className="sorts">Price Ascending</button>
+					</Link>
+					<form onSubmit={this.handleSearch}>
+						<input
+							className="title-search"
+							placeholder="Search Title..."
+							type="text"
+							value={this.state.search}
+							onChange={(event) => this.setState({ search: event.target.value })}
+						/>
+						<button className="search-button" type="submit">
+							<i className="fas fa-search" />
+						</button>
+					</form>
+					<div className="properties">
+						{this.state.properties.map((property) => <PropertyCard key={property._id} {...property} />)}
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default Properties;
